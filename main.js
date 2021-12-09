7dec407cbb945dbec6a2b51c845953cc75d0107e
Nikita <ussnikita209@mail.ru>
2021-12-09 14:03:00 +0300

    change root

---
+++ main.js
@@ -0,0 +1,105 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.html":
+/*!********************!*\
+  !*** ./index.html ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// Module
+var code = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n  <link rel=\"shortcut icon\" href=\"./assets/favicon.ico\" type=\"image/x-icon\">\r\n  <title>Jesus page</title>\r\n</head>\r\n\r\n<body>\r\n  <header class=\"header\">\r\n    <div class=\"header__container container\">\r\n      <div class=\"header__site-name\">\r\n        <span>Sitename</span>\r\n      </div>\r\n      <ul class=\"navigator\">\r\n        <li class=\"navigator__item\"><a href=\"#\">Home</a></li>\r\n        <li class=\"navigator__item\"><a href=\"#\">About</a></li>\r\n        <li class=\"navigator__item\"><a href=\"#\">Services</a></li>\r\n        <li class=\"navigator__item\"><a href=\"#\">Work</a></li>\r\n        <li class=\"navigator__item\"><a href=\"#\">Contact</a></li>\r\n      </ul>\r\n    </div>\r\n  </header>\r\n  <main>\r\n    <section class=\"wrapper_dark\">\r\n      <div class=\"container welcome \">\r\n        <h1 class=\"welcome__title\">I am the way, the truth, and the life.</h1>\r\n        <p class=\"welcome__text\">No one comes to the Father except through me.</p>\r\n      </div>\r\n      <div class=\"image\">\r\n        <div class=\"image__container\">\r\n        </div>\r\n      </div>\r\n    </section>\r\n    <section class=\"container\">\r\n      <ul class=\"row\">\r\n        <li class=\"row__column\">\r\n          <div class=\"quote\">\r\n            <div class=\"quote__icon icon\"><img id=\"heart-icon\" class=\"icon__container\" src=\"./assets/img/heart.webp\"\r\n                alt=\"Heart picture\"></div>\r\n            <h2 class=\"quote__title\">Chronicles 16:11</h2>\r\n            <p class=\"quote__text\">Seek the LORD and his strength; seek his presence continually!\r\n            </p>\r\n            <a class=\"quote__more\" href=\"#\"><img src=\"./assets/img/arrow.ico\" alt=\"Arrow picture\"></a>\r\n          </div>\r\n        </li>\r\n        <li class=\"row__column\">\r\n          <div class=\"quote\">\r\n            <div class=\"quote__icon icon\"><img class=\"icon__container\" src=\"./assets/img/pencil.webp\"\r\n                alt=\"Pencil picture\"></div>\r\n            <h2 class=\"quote__title\">Romans 12:12</h2>\r\n            <p class=\"quote__text\">Rejoice in hope, be patient in tribulation, be constant in prayer.\r\n            </p>\r\n            <a class=\"quote__more\" href=\"#\"><img src=\"./assets/img/arrow.ico\" alt=\"Arrow picture\"></a>\r\n          </div>\r\n        </li>\r\n        <li class=\"row__column\">\r\n          <div class=\"quote\">\r\n            <div class=\"quote__icon icon\"><img class=\"icon__container\" src=\"./assets/img/flag.webp\" alt=\"Flag picture\">\r\n            </div>\r\n            <h2 class=\"quote__title\">Philippians 4:13 </h2>\r\n            <p class=\"quote__text\">I can do all things through him who strengthens me.\r\n            </p>\r\n            <a class=\"quote__more\" href=\"#\"><img src=\"./assets/img/arrow.ico\" alt=\"Arrow picture\"></a>\r\n          </div>\r\n        </li>\r\n      </ul>\r\n    </section>\r\n    <section class=\"container articles\">\r\n      <article class=\"article\">\r\n        <div class=\"article__icon article__icon_child\"></div>\r\n        <div class=\"article__content content\">\r\n          <h2 class=\"content__title\">Cestib umauct</h2>\r\n          <p class=\"content__text\">Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida\r\n            non,\r\n            comm lorem ipin <a class=\"link\" href=\"#\">read more ...</a></p>\r\n        </div>\r\n      </article>\r\n      <article class=\"article article_reverse\">\r\n        <div class=\"article__icon article__icon_birds\"></div>\r\n        <div class=\"article__content content content_left\">\r\n          <h2 class=\"content__title\">Nunc dignissim</h2>\r\n          <p class=\"content__text\">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque\r\n            volutpat matti <a href=\"#\" class=\"link\">read more ...</a></p>\r\n        </div>\r\n      </article>\r\n      <article class=\"article\">\r\n        <div class=\"article__icon article__icon_jesus\"></div>\r\n        <div class=\"article__content content\">\r\n          <div class=\"content__title\">Cestib umauct</div>\r\n          <div class=\"content__text\">Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida\r\n            non, comm lorem ipin <a class=\"link\" href=\"#\">read more ...</a></div>\r\n        </div>\r\n      </article>\r\n      <article class=\"article article_reverse\">\r\n        <div class=\"article__icon article__icon_tree\"></div>\r\n        <div class=\"article__content content content_left\">\r\n          <div class=\"content__title\">Nunc dignissim</div>\r\n          <div class=\"content__text\">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque\r\n            volutpat matti <a class=\"link\" href=\"#\">read more ...</a></div>\r\n        </div>\r\n      </article>\r\n    </section>\r\n    <div class=\"wrapper_dark\">\r\n      <section class=\"container contacts\">\r\n        <div class=\"contact\">\r\n          <div class=\"contact__icon icon icon_small\"><img id=\"plane\" class=\"icon__container\"\r\n              src=\"./assets/img/paper-plane.webp\" alt=\"Contact picture\"></div>\r\n          <div class=\"contact__content\">\r\n            <p class=\"contact__text\">faceboom.com/sitename</p>\r\n            <p class=\"contact__text\">@sitename</p>\r\n          </div>\r\n        </div>\r\n        <div class=\"contact\">\r\n          <div class=\"contact__icon icon icon_small\"><img id=\"micro\" class=\"icon__container\"\r\n              src=\"./assets/img/micro.webp\" alt=\"Contact picture\"></div>\r\n          <div class=\"contact__content\">\r\n            <p class=\"contact__text\">“ Donec consectetuer vulputate se\r\n              m tristiq donec odio com ”</p>\r\n          </div>\r\n        </div>\r\n        <div class=\"contact\">\r\n          <div class=\"contact__icon icon icon_small\"><img id=\"user\" class=\"icon__container\" src=\"./assets/img/user.webp\"\r\n              alt=\"Contact picture\"></div>\r\n          <div class=\"contact__content\">\r\n            <p class=\"contact__text\">Email : info@sitename.com</p>\r\n            <p class=\"contact__text\">Call Us : (000) 777 777 7777</p>\r\n          </div>\r\n        </div>\r\n      </section>\r\n    </div>\r\n  </main>\r\n  <footer class=\"wrapper_dark\">\r\n    <div class=\"container\">\r\n      <form class=\"subscribe\" action=\"\">\r\n        <label class=\"subscribe__title\" for=\"\">Vivamus vesinqu</label>\r\n        <div class=\"subscribe__field\">\r\n          <input class=\"subscribe__input\" type=\"text\" placeholder=\"your email id here...\">\r\n          <button class=\"subscribe__button\">Subscribe</button>\r\n        </div>\r\n      </form>\r\n      <div class=\"sign\">\r\n        <p class=\"sign__text\">Copyright(c) website name</p>\r\n        <p class=\"sign__text\"><a class=\"link\" href=\"#\">Designed by: www.alltemplateneeds.com</a> / Images from:\r\n          www.wallpaperswide.com</p>\r\n      </div>\r\n    </div>\r\n  </footer>\r\n</body>\r\n\r\n</html>";
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);
+
+/***/ }),
+
+/***/ "./scss/style.scss":
+/*!*************************!*\
+  !*** ./scss/style.scss ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/style.scss */ "./scss/style.scss");
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.html */ "./index.html");
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
