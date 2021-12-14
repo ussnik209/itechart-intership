6a5d10e523a1bed641b3f61bdd26ec6ce699ee37
Nikita <ussnikita209@mail.ru>
2021-12-14 22:52:17 +0300

    Task 2: Date Display Formatter - implementing formatting with text. add: array of months. add: transformation formatted date to date with text

--- src/js/DateDisplayFormatter.js
+++ src/js/DateDisplayFormatter.js
@@ -1,17 +1,33 @@
 let DateDisplayFormatter = {
-  format(dateStr, inputExpr = 'DDMMYYYY', outputExpr = 'DD-MM-YYYY') {
-    if (typeof dateStr !== 'string') dateStr = String(dateStr)
+  months: ['January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ],
 
+  format(dateStr, { inputExpr, outputExpr, isText }) {
+    inputExpr = inputExpr || 'DDMMYYYY'
+    outputExpr = outputExpr || 'DD-MM-YYYY'
+    isText = isText || false
+
+    if (typeof dateStr !== 'string') dateStr = String(dateStr)
     let { day, month, year } = this.getParsedDate(dateStr, inputExpr.split(''))
     let formattedDate = this.getFormattedDate(outputExpr.split(''),
       day.split(''),
       month.split(''),
       year.split(''))
 
+      
+      if (isText) {
+        formattedDate = this.toText(formattedDate.split(' '), outputExpr.split(' ')).join(' ')
+      }
+      console.log(formattedDate)
 
+    return formattedDate
   },
 
-  getParsedDate(date, expr) {
+  getParsedDate(dateStr, exprArr) {
+    let expr = exprArr,
+      date = dateStr
     let year = '',
       month = '',
       day = ''
@@ -37,27 +53,42 @@ let DateDisplayFormatter = {
     }
   },
 
-  getFormattedDate(expr, dayInput, monthInput, yearInput) {
-    let day = dayInput,
-      month = monthInput,
-      year = yearInput
-    expr.reverse()
-    
+  getFormattedDate(exprArr, dayInputArr, monthInputArr, yearInputArr) {
+    let expr = exprArr.reverse()
+    let day = dayInputArr,
+      month = monthInputArr,
+      year = yearInputArr
+
     expr = expr.map(el => {
       switch (el) {
         case 'D':
           return day.pop()
         case 'M':
           return month.pop()
-        case 'Y': 
+        case 'Y':
           return year.pop()
         default:
           return el
       }
     })
 
-    return expr.reverse()
+    return expr.reverse().join('')
+  },
+
+  toText(dateArr, exprArr) {
+    let date = dateArr,
+      expr = exprArr
+
+    expr.forEach((el, i) => {
+      if (el[0] === 'M') date[i] = this.monthToText(date[i])
+    })
+
+    return date
+  },
+
+  monthToText(month) {
+    return this.months[month]
   }
 }
 
-// let res = DateDisplayFormatter.format('31102011')
+let res = DateDisplayFormatter.format('31102011', { isText: true, outputExpr: 'DD MM YYYY' })
