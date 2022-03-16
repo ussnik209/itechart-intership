e1b4387117b4447226d7e049b3a1bc249fa240cd
Nikita <ussnikita209@mail.ru>
2022-03-16 15:55:41 +0300

    ReactJS Tasks - implementing simulation of getting ingredients. add: accordion interface to menuitem

--- src/app/components/MenuItem.jsx
+++ src/app/components/MenuItem.jsx
@@ -1,13 +1,28 @@
-import { Card, CardActions } from '@mui/material'
 import React from 'react'
 
+import { 
+  Card, 
+  CardActions, 
+  Accordion, 
+  AccordionDetails, 
+  AccordionSummary 
+} from '@mui/material'
+
 import Dish from './Dish.jsx'
+
 
 const MenuItem = ({dish, Button}) => (
   <Card sx={{
     width: 1
   }}>
-    <Dish {...dish}/>
+    <Accordion>
+      <AccordionSummary>
+        <Dish {...dish}/>
+      </AccordionSummary>
+      <AccordionDetails>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium eaque impedit odio possimus eveniet sit adipisci cumque recusandae dolore unde laborum libero esse explicabo sunt molestias, eligendi blanditiis! Eum, odit!
+      </AccordionDetails>
+    </Accordion>
     <CardActions>
       {
         Button 
